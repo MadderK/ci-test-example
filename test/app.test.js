a09c4eae2c136c9066b3143af2852cc399f0b9c1
Xuperman <160150215+MadderK@users.noreply.github.com>
2025-05-20 14:31:06 +0800

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,14 @@
-javascript
-const add = require('../app');
-const { expect } = require('chai');
+// app.test.js
+let add;
+let expect;
+
+before(async () => {
+  add = (await import('../app.js')).default; // 动态导入默认导出
+  expect = (await import('chai')).expect;     // 动态导入 expect
+});
 
 describe('Add function', () => {
-    it('should return 5 when adding 2 and 3', () => {
-        expect(add(2, 3)).to.equal(5);
-    });
+  it('should return 5 when adding 2 and 3', () => {
+    expect(add(2, 3)).to.equal(5);
+  });
 });
